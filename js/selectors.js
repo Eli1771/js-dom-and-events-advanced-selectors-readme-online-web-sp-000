f0ebfd4a5b6747f6c951a653073d05fb49fd5e3c
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 21:58:05 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,19 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {
+  
+}
+
+function ninjaBabySelector() {
+  
+}
+
+function divSelector() {
+  
+}
+
+function firstListItem() {
+  
+}
